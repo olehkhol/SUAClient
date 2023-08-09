954ba1505a423b1e66642519b27a4f4639eb32fe
Oleh Kholiavchuk <oleh.khol@gmail.com>
2023-08-09 20:34:54 +0300

    14. Navigation Setup

--- app/src/main/java/sky/tavrov/suaclient/MainActivity.kt
+++ app/src/main/java/sky/tavrov/suaclient/MainActivity.kt
@@ -3,13 +3,6 @@ package sky.tavrov.suaclient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import sky.tavrov.suaclient.navigation.SetupNavController
 import sky.tavrov.suaclient.ui.theme.SUAClientTheme
@@ -22,30 +15,7 @@ class MainActivity : ComponentActivity() {
                 val navController = rememberNavController()
 
                 SetupNavController(navController = navController)
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SUAClientTheme {
-        Greeting("Android")
     }
 }
