a76bc0d42467f910ea4ffd6a6547fd39c0329452
Oleh Kholiavchuk <oleh.khol@gmail.com>
2023-08-23 10:57:41 +0300

    Update SDK to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "sky.tavrov.suaclient"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "sky.tavrov.suaclient"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
